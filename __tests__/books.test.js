5b1976b63fd1604107bb8ddff70a916f52261a87
skyla <nathanchrispearce@gmail.com>
2024-05-18 18:07:11 -0600

    fixed tests

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -13,7 +13,7 @@ beforeEach(async () => {
     INSERT INTO books
     (isbn, amazon_url, author, language, pages, publisher, title, year)
     VALUES(
-        '8976568',
+        '111',
         'https://amazon.com/not-real',
         'me',
         'English',
@@ -56,23 +56,12 @@ describe("POST /books", () => {
 
 describe("PUT /books", () => {
     test('updates book', async () => {
-        //adds a book
-        await request(app)
-        .post(`/books`)
-        .send({
-            isbn: '8976568',
-            amazon_url: 'https://amazon.com/not-real',
-            author: 'me',
-            language: 'English',
-            pages: 330,
-            publisher: 'Not real publishers',
-            title: 'This is a book :D',
-            year: 2024
-        });
+        // checks that book is in db
+        const books = await db.query("SELECT * FROM books")
+        console.log(books.rows[0])
 
-        // tests PUT
         const response = await request(app)
-            .post(`/books/8976568`)
+            .put(`/books/111`)
             .send({
                 amazon_url: 'https://amazon.com/definitely-not-real',
                 author: 'not me :(',
@@ -82,13 +71,13 @@ describe("PUT /books", () => {
                 title: 'This is a book :D',
                 year: 2027
             });
-        expect(response.status).toBe(201);
+        expect(response.status).toBe(200);
         expect(response.body.book).toHaveProperty("isbn");
     })
 
     test('invalid entry', async () => {
         const response = await request(app)
-            .post(`/books/8976568`)
+            .put(`/books/111`)
             .send({
                 year: 2024
             });
